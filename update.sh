ae0620e3c44b17e03a6fc9e51a8a5f9cd2c170b1
Kent Ross <k@mad.cash>
2024-05-26 03:23:24 -0700

    easy update script

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [[ -z "$1" ]]
+then
+  echo "USAGE: ./update.sh VERSION"
+  echo "...where VERSION is the 7-digit sqlite version, like 3450300"
+  exit 2
+fi
+
+set -euxo pipefail
+
+TEMP=$(mktemp)
+curl "https://sqlite.org/2024/sqlite-amalgamation-${1}.zip" > $TEMP
+rm -r sqlite-amalgamation-*
+unzip $TEMP -d .
+sed -i "s/^sqlite3_folder = .*/sqlite3_folder = '$(ls -d sqlite-amalgamation-*)'/" meson.build
+echo ok
